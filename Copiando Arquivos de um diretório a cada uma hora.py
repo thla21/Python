5264f0814b30dea5e046bb7b1a876cc233f84622
Thiago Henrique <62508225+thla21@users.noreply.github.com>
2024-04-04 17:45:39 -0300

    Update Copiando Arquivos de um diretório a cada uma hora.py

--- Copiando Arquivos de um diretório a cada uma hora.py
+++ Copiando Arquivos de um diretório a cada uma hora.py
@@ -6,7 +6,7 @@ diretorio_origem = 'C:\\caminho\do\diretorio_origem'                  # Diretór
 diretorio_destino = 'C:\\caminho\do\diretorio_destino'                # Diretório de destino
 
 
-def clonar_arquivos(origem, destino):                              # Criamos uma nova função para clonar arquivos de um diretório para outro
+def clonar_arquivos(origem, destino):                                 # Criamos uma nova função para clonar arquivos de um diretório para outro
     for root, dirs, files in os.walk(origem):
         for file in files:
             origem_arquivo = os.path.join(root, file)
@@ -15,10 +15,10 @@ def clonar_arquivos(origem, destino):                              # Criamos uma
             print(f'Arquivo {file} clonado com sucesso!')
 
 
-def verificar_novos_arquivos(diretorio):                           # Criamos uma nova função para verificar novos arquivos no diretório
+def verificar_novos_arquivos(diretorio):                             # Criamos uma nova função para verificar novos arquivos no diretório
     arquivos_anteriores = set(os.listdir(diretorio))
     while True:
-        time.sleep(3600)                                           # Espera 1 hora (3600 segundos)
+        time.sleep(3600)                                             # Espera 1 hora (3600 segundos)
         arquivos_atuais = set(os.listdir(diretorio))
         novos_arquivos = arquivos_atuais - arquivos_anteriores
         if novos_arquivos:
@@ -28,7 +28,7 @@ def verificar_novos_arquivos(diretorio):                           # Criamos uma
             arquivos_anteriores = arquivos_atuais
 
 
-clonar_arquivos(diretorio_origem, diretorio_destino)               # Clonar os arquivos inicialmente
+clonar_arquivos(diretorio_origem, diretorio_destino)                 # Clonar os arquivos inicialmente
 
 
-verificar_novos_arquivos(diretorio_origem)                         # Iniciar verificação de novos arquivos a cada hora
+verificar_novos_arquivos(diretorio_origem)                           # Iniciar verificação de novos arquivos a cada hora

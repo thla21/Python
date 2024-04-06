50d848a0c1db3b472c758058d8bd27672c0164ff
Thiago Henrique <62508225+thla21@users.noreply.github.com>
2024-04-06 06:30:13 -0300

    Update Configurando Interface de Rede com IP Manual no Windows.py

--- Configurando Interface de Rede com IP Manual no Windows.py
+++ Configurando Interface de Rede com IP Manual no Windows.py
@@ -1,32 +1,23 @@
 import wmi
 
-def configurar_ip_manual(interface, endereco_ip, mascara_subrede, gateway):                                  # Conectar ao serviço WMI do Windows
+def configurar_ip_manual(interface, endereco_ip, mascara_subrede, gateway):                   # Conectar ao serviço WMI do Windows
     try:
-        
         c = wmi.WMI()
-
-        # Obter a interface de rede especificada
-        nic = c.Win32_NetworkAdapterConfig(IPEnabled=True, Description=interface)[0]
-
-        # Configurar o endereço IP manualmente
-        retorno = nic.EnableStatic(IPAddress=[endereco_ip], SubnetMask=[mascara_subrede])
-        
-        # Configurar o gateway
-        gateway_config = nic.SetGateways(DefaultIPGateway=[gateway])
-        
+        nic = c.Win32_NetworkAdapterConfig(IPEnabled=True, Description=interface)[0]          # Obter a interface de rede especificada
+        retorno = nic.EnableStatic(IPAddress=[endereco_ip], SubnetMask=[mascara_subrede])     # Configurar o endereço IP manualmente
+        gateway_config = nic.SetGateways(DefaultIPGateway=[gateway])                          # Configurar o gateway
         if retorno[0] == 0 and gateway_config[0] == 0:
             print("Configuração de IP manual bem-sucedida.")
         else:
             print("Erro ao configurar IP manualmente.")
-
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
 
 # Defina as informações da interface de rede e o endereço IP manual aqui
-interface = "Sua Interface de Rede"  # Exemplo: "Ethernet"
-endereco_ip = "192.168.1.100"
-mascara_subrede = "255.255.255.0"
-gateway = "192.168.1.1"
+interface = "Sua Interface de Rede"                                                          # Exemplo: "Ethernet"
+endereco_ip = "192.168.1.100"                                                                # Endereço IP do host.
+mascara_subrede = "255.255.255.0"                                                            # Máscara de rede.
+gateway = "192.168.1.1"                                                                      # Gateway para conexão com a Internet
 
-# Chame a função para configurar o IP manualmente
-configurar_ip_manual(interface, endereco_ip, mascara_subrede, gateway)
+
+configurar_ip_manual(interface, endereco_ip, mascara_subrede, gateway)                       # Chame a função para configurar o IP manualmente       

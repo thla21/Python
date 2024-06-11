4a93957ea23f5b9593e830e8957b2ce97b0eab34
thla21 <thiagohenriquela.21@gmail.com>
2024-06-11 04:30:20 -0300

    Update with erro on append

---
+++ Projetos/app_mobile/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+CONN = "sqlite:///projeto2.db"
+
+engine = create_engine(CONN, echo = True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Produto(Base):
+    __tablename__ = "Produto"
+    id = Column(Integer, primary_key=True)
+    titulo = Column(String(50))
+    preco = Column(Float())
+
+Base.metadata.create_all(engine)
